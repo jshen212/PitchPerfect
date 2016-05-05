cb16a87e0fa4f6f50e23381b6e848df2af381c16
simplejeff <jeffshen86@gmail.com>
2016-05-05 15:34:44 -0700

    [feature] built out playback with special effects

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
@@ -18,12 +19,48 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var reverb: UIButton!
     @IBOutlet weak var stop: UIButton!
     
+    @IBAction func stopButtonPressed(sender: AnyObject) {
+        print("Stop audio button pressed")
+    }
+    
+    enum ButtonType: Int { case snail = 0, rabbit, chipmunk, darth, parrot, reverb }
+    
+    @IBAction func playSoundForButton(sender: UIButton) {
+        print("Play sound button pressed")
+        switch(ButtonType (rawValue: sender.tag)!){
+        case .snail:
+            playSound(rate: 0.5)
+        case .rabbit:
+            playSound(rate:1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .darth:
+            playSound(pitch: -1000)
+        case .parrot:
+            playSound(echo: true)
+        case .reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.Playing)
+    }
+    
+    var recordedAudio : NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("playSoundsViewController loaded")
+        setupAudio()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        configureUI(.NotPlaying)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
