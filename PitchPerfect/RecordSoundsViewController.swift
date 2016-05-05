926e97faaec7372f7b2a0f8c9312a61a7dfde014
simplejeff <jeffshen86@gmail.com>
2016-05-05 13:21:22 -0700

    [feature] added functionality to record and pass sounds to the playSoundsViewController

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -70,6 +70,15 @@ class RecordSoundsViewController: UIViewController , AVAudioRecorderDelegate {
             print("Saving of recording failed")
         }
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "stopRecording"){
+            let playSoundsVC = segue.destinationViewController as!
+                PlaySoundsViewController
+            let recordedAudioURL = sender as! NSURL
+            playSoundsVC.recordedAudio = recordedAudioURL
+        }
+    }
 
 }
 
